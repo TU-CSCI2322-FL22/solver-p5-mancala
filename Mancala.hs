35355714b252bf41adb3ff7fa3348c59b01d572f
2021-08-24 <bmach@xena24.cs.trinity.edu>
2022-11-07 10:05:55 -0600

    showBoard working

--- Mancala.hs
+++ Mancala.hs
@@ -1,3 +1,6 @@
+import Debug.Trace
+import Data.Maybe
+
 type Bean = Int
 type Slot = Bean 
 data Player = P1 | P2 deriving (Show, Eq)
@@ -5,23 +8,22 @@ data Board = Board {goalP1 :: Slot, slotsP1 :: [Slot],
                     goalP2 :: Slot, slotsP2 :: [Slot], playerTurn :: Player} deriving (Show, Eq) --add record notation 
 data Outcome = Turn | Winner Player | Tie 
 
-board = Board {goalP1 = 0, slotsP1 = [4,4,4,4,4,4], goalP2 = 0, slotsP2 = [4,4,4,4,4,4], playerTurn = P1}
+board = Board {goalP1 = 0, slotsP1 = [4,4,4,4,4,4], goalP2 = 0, slotsP2 = [4,4,4,4,4,4], playerTurn = P2}
 
 --Show Function
 ------------------------------------
 -- ha ha funny show function
 showBoard :: Board -> String
-showBoard (g1,s1,g2,s2,(Player P1)) = "    (6) (5) (4) (3) (2) (1)\n" ++ body
-showBoard (g1,s1,g2,s2,(Player P2)) = border ++ displaySideOne s1++ "|"++show g1++" |-------    ----------------| "++show g2++"|\n" ++ displaySideTwo s2 ++ border ++ "   (1) (2) (3) (4) (5) (6)\n"
+showBoard Board {goalP1 = g1, slotsP1 = s1, goalP2 = g2, slotsP2 = s2, playerTurn = p} =if p == P1 then  "    (6) (5) (4) (3) (2) (1) \n" ++ body else body ++ "    (1) (2) (3) (4) (5) (6)\n"
   where border = "@><><><><><><><@><><><><><><><@\n"
-        body = border ++ "| |"++displaySideOne s1++"| |\n"++ "|"++show g1++    " |-----------------------| "++show g2++"|\n" ++ "| |"++displaySideTwo s2 ++"| |\n"++ border
+        body = border ++ "|  "++displaySideOne s1++"|  |\n"++ "|"++show g1++    " |-----------------------| "++show g2++"|\n" ++ "|  "++displaySideTwo s2 ++"|  |\n"++ border
 
 
 displaySideOne :: [Slot] -> String
 displaySideOne [] = ""
 displaySideOne (b:bs) = displaySideOne bs ++ "| "++show b++" "
 
-displaySideTwo :: Board -> String
+displaySideTwo :: [Slot] -> String
 displaySideTwo [] = ""
 displaySideTwo (b:bs) = "| "++show b++" "++displaySideTwo bs
 
