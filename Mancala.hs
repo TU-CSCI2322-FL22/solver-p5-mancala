1162ee2d86efbbdf45a69ca499f8a97a00d4162c
dreckTrinity <99363112+dreckTrinity@users.noreply.github.com>
2022-11-09 11:45:28 -0600

    Capture works

--- Mancala.hs
+++ Mancala.hs
@@ -35,19 +35,22 @@ displaySideTwo (b:bs) = "| "++show b++" "++displaySideTwo bs
 across :: Int -> Int
 across x = if x == 7 then 14 else 14 - x
 
---Board {goalP1 = g1, slotsP1 = s1, goalP2 = g2, slotsP2 = s2, playerTurn = p}
--- pattern match and return the opposite slot for every position
-move :: Board -> Int -> Board
-move brd pos = executePlay  (beans)
-  where brkdn = breakdown brd
-        slots = getSide brkdn
-        (s, beans) = insideMovement slots pos
+----Board {goalP1 = g1, slotsP1 = s1, goalP2 = g2, slotsP2 = s2, playerTurn = p}
+---- pattern match and return the opposite slot for every position
+--move :: Board -> Int -> Board
+--move brd pos = executePlay  (beans)
+--  where brkdn = breakdown brd
+--        slots = getSide brkdn
+--        (s, beans) = insideMovement slots pos
 
 breakdown :: Board -> FauxBoard
 breakdown Board {goalP1 = g1, slotsP1 = s1, goalP2 = g2, slotsP2 = s2, playerTurn = p} = (g1,s1,g2,s2,p)
 
 getSide :: FauxBoard -> [Slot]
 getSide (g1,s1,g2,s2,p) = if p == P1 then s1 else s2
+
+getOtherSide :: FauxBoard -> [Slot]
+getOtherSide (g1,s1,g2,s2,p) = if p == P1 then s2 else s1
 
 --Moves the Peices on one side of the board, does not delete the starting peice
 --First int is position, scond int is beans
@@ -64,31 +67,56 @@ splitAndRebuild (b:bs) beans = (b+1):(splitAndRebuild bs (beans-1))
 
 
 --Doesnt work for bigger boards than standard
-insideMovement :: [Slot] -> Int -> ([Slot], Bean)
-insideMovement slots pos = newFront front ++ sideMovement back (pos-1) beans
-  where splice = splitAt pos slots
-        front = fst splice
-        back = snd splice
-        beans = last front
+--insideMovement :: [Slot] -> Int -> ([Slot], Bean)
+--insideMovement slots pos = newFront front ++ sideMovement back (pos-1) beans
+--  where splice = splitAt pos slots
+--        front = fst splice
+--        back = snd splice
+--        beans = last front
+--
+--newFront :: [Slot] -> [Slot]
+--newFront [b] = [0]
+--newFront (b:bs) = b:(newFront bs)
 
-newFront :: [Slot] -> [Slot]
-newFront [b] = [0]
-newFront (b:bs) = b:(newFront bs)
 
 
-
-executePlay :: Board -> Beans -> Board
-exectuePlay brd pl (slots, beans) = if beans > 1 then
+executePlay :: Board -> Bean -> Board
+executePlay brd pl = undefined
 --Board {goalP1 = g1, slotsP1 = s1, goalP2 = g2, slotsP2 = s2, playerTurn = p} pos = if 7 - pos == num then --goes in goal
 --       else if 7 - pos < num then  --stays on p1 side
 --       else --goes to p2 side
 
-checkCapture :: Board -> Bool
-checkCapture = undefined
+checkCapture :: Board -> Int -> Board
+checkCapture brd pos =
+  let fauxBrd = breakdown brd
+      currentSide = getSide fauxBrd
+  in if (currentSide !! (pos - 1)) == 1 then capture brd pos else brd
 
 -- takes a board and a position and return an updated board
-capture :: Board -> Board
-capture = undefined
+capture :: Board -> Int -> Board
+capture brd pos =
+  let fauxBrd = breakdown brd
+      currentSide = getSide fauxBrd
+      otherSide = getOtherSide fauxBrd
+      goal1 (g1,_,_,_,p) beans = if p == P1 then g1 + beans else g1
+      goal2 (_,_,g2,_,p) beans = if p == P2 then g2 + beans else g2
+      beanTotal = 1 + (otherSide !! (pos-1))
+  in  Board (goal1 fauxBrd beanTotal) (makePosZero (getSide1 fauxBrd) pos) (goal2 fauxBrd beanTotal) (makePosZero (getSide2 fauxBrd) pos) (getPlayer (fauxBrd))
+
+getPlayer :: FauxBoard -> Player
+getPlayer (_,_,_,_,p) = p
+
+getSide1 :: FauxBoard -> [Slot]
+getSide1 (_,s1,_,_,_) = s1
+
+getSide2 :: FauxBoard -> [Slot]
+getSide2 (_,_,_,s2,_) = s2
+
+makePosZero :: [Slot] -> Int -> [Slot]
+makePosZero slots pos = frontHalf ++ [0] ++ backHalf
+  where frontHalf = tail (fst(splitAt (pos) slots))
+        backHalf = snd(splitAt (pos) slots)
+
 
 -- checks if it is possible to capture a piece and if so changs the board
 
